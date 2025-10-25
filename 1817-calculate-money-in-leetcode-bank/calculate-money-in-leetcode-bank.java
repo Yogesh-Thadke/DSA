4cac87cb57ca0a89906cb24ef6837cec765b4cff
Yogesh-Thadke <yogeshthadke22@gmail.com>
2025-10-25 23:35:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (22.22%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -3,8 +3,9 @@ class Solution {
         int k = n/7;
         int r = n%7;
         int next = k+1;
-        
-        int ans = (k*(56+7*(k-1))/2)+(r*(2*next+(r-1))/2);
+        int fullSum = k*(56+7*(k-1))/2;
+        int remSum = r*(2*next+(r-1))/2;
+        int ans = fullSum+remSum;
         return ans;
     }
 }
