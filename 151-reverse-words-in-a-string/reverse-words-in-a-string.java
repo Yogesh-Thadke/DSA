8db1a96e6632da231e9e38379d9c11822c96063c
Yogesh-Thadke <yogeshthadke22@gmail.com>
2025-11-26 22:15:26 +0530

    Time: 7 ms (58.87%) | Memory: 44.3 MB (35.96%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,47 +1,18 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb = trimSpace(s);
-        reverse(sb, 0, sb.length()-1);
-        reverseWord(sb);
-        return sb.toString();
-
-    }
-    private StringBuilder trimSpace(String s){
-        int l=0, r=s.length()-1;
-        while(l<=r && s.charAt(l)==' ')l++;
-        while(r>=l && s.charAt(r)==' ')r--;
-
+        s = s.trim();
+        if(s.length()==0){
+            return s;
+        }
         StringBuilder sb = new StringBuilder();
-
-        while(l<=r){
-            char c = s.charAt(l);
-
-            if(c!=' '){
-                sb.append(c);
-            }
-            else if(sb.charAt(sb.length()-1)!=' '){
-                sb.append(' ');
-            }
-            l++;
-        }
-        return sb;
-    } 
-    private void reverse(StringBuilder st, int l, int r){
-        while(l<r){
-            char temp = st.charAt(l);
-            st.setCharAt(l,st.charAt(r));
-            st.setCharAt(r,temp);
-            l++;
-            r--;
-        }
-    }
-    private void reverseWord(StringBuilder st){
-        int start = 0;
-        for(int i=0; i<=st.length(); i++){
-            if(i==st.length() || st.charAt(i)==' '){
-                reverse(st, start, i-1);
-                start = i+1;
+        String[] st = s.split(" +");
+        int n = st.length;
+        for(int i = n-1; i>=0; i--){
+            sb.append(st[i]);
+            if(i>0){
+                sb.append(" ");
             }
         }
+        return sb.toString();
     }
 }
