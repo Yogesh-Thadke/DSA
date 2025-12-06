af32dd5f57fe53d7f5db2cb31ab443cea31fc5c6
Yogesh-Thadke <yogeshthadke22@gmail.com>
2025-12-06 14:50:07 +0530

    Time: 42 ms (69.93%) | Memory: 134.2 MB (65.76%) - LeetSync

--- 4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.java
+++ 4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.java
@@ -1,38 +1,11 @@
 class Solution {
     public int countElements(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
         Arrays.sort(nums);
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-            if(i==0 || nums[i]!=nums[i-1]){
-                list.add(nums[i]);
-            }
-        }
-        
-        for(int i=0; i<list.size(); i++){
-            int indexFirstGreater = upperBound(nums, list.get(i));
-            int firstGreater = nums.length-indexFirstGreater;
-            map.put(list.get(i), firstGreater);
-        }
-        int count = 0;
+        if(k==0) return nums.length;
+        int upperBound = nums[nums.length-k], ans=0;
         for(int num : nums){
-            if(map.get(num)>=k){
-                count++;
-            }
+            if(num < upperBound) ans++;
         }
-        return count;
-    }
-    public int upperBound(int[] nums, int num){
-        int l=0, r=nums.length;
-        while(l<r){
-            int mid=l+(r-l)/2;
-            if(nums[mid] <= num){
-                l=mid+1;
-            }
-            else{
-                r=mid;
-            }
-        }
-        return l;
+        return ans;
     }
 }
