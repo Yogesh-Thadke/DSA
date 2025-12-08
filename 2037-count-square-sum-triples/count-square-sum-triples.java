055ec881bf606ebc51523f3289aec1bea5af0593
Yogesh-Thadke <yogeshthadke22@gmail.com>
2025-12-09 00:24:50 +0530

    Time: 47 ms (26.10%) | Memory: 46.4 MB (5.22%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -1,10 +1,11 @@
 class Solution {
     public int countTriples(int n) {
         int count=0;
-        for(int i=1; i<=n-1; i++){
-            for(int j=i+1; j<=n; j++){
-                int ans = (int)Math.sqrt(i*i+j*j);
-                if(ans<=n && (ans*ans) == (i*i+j*j)) count+=2;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=1; i<=n; i++)set.add(i*i);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(set.contains((i*i)+(j*j)))count++;
             }
         }
         return count;
