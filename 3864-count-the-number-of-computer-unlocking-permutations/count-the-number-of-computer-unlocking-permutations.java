567077a46e56097bb60dedce2dafaa9c5e12f9ec
Yogesh-Thadke <yogeshthadke22@gmail.com>
2025-12-10 11:12:13 +0530

    Time: 1 ms (100.00%) | Memory: 92.4 MB (5.33%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.java
@@ -1,4 +1,5 @@
 class Solution {
+    static final int mod = 1000000007;
     public int countPermutations(int[] complexity) {
         int n = complexity.length;
         for(int i=1; i<n; i++){
@@ -6,7 +7,7 @@ class Solution {
                 return 0;
             }
         }
-        int mod = 1000000007;
+        
         long ans = 1;
         for(int k=1; k<n; k++){
             ans = (ans*k)%mod;
