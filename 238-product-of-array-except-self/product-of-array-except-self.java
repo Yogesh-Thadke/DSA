f51cd4a3324a720be1dc8e4d58db717577a33874
Yogesh-Thadke <yogeshthadke22@gmail.com>
2026-03-21 19:02:45 +0530

    Time: 2 ms (94.66%) | Memory: 64.9 MB (85.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,22 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
-        left[0]=nums[0];
-        left[1]=nums[0];
-        for(int i=2; i<nums.length; i++){
-            left[i]=nums[i-1]*left[i-1]; 
+        int n = nums.length;
+        int[] left = new int[n];
+        left[0] = 0;
+        left[1] = nums[0];
+        for(int i=2; i<n; i++){
+            left[i]=left[i-1]*nums[i-1];
         }
-        right[nums.length-1]=nums[nums.length-1];
-        right[nums.length-2]=nums[nums.length-1];
-        for(int i=nums.length-3; i>=0; i--){
-            right[i] = nums[i+1]*right[i+1];
+
+        int[] right = new int[n];
+        right[n-1] = 0;
+        right[n-2] = nums[n-1];
+
+        for(int i=n-3; i>=0; i--){
+            right[i]=right[i+1]*nums[i+1];
         }
-        left[0]=right[0];
-        for(int i=1; i<nums.length-1; i++){
-            left[i] = left[i]*right[i];
+        int[] result = new int[n];
+        result[0] = right[0];
+        result[n-1] = left[n-1];
+
+        for(int i=1; i<n-1; i++){
+            result[i] = left[i]*right[i];
         }
-        return left;
+
+        return result;
     }
-    
 }
