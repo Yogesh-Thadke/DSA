3f6e7b1832a86865ab36014d455db6c9cb93dfa8
Yogesh-Thadke <yogeshthadke22@gmail.com>
2026-06-22 11:13:12 +0530

    Time: 8 ms (49.83%) | Memory: 43.4 MB (71.44%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,28 +1,20 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        HashMap<Character,Integer> freq = new HashMap<>();
-        char[] sample = {'b','a','l','o','n'};
+        int freq_b = 0;
+        int freq_a = 0;
+        int freq_l = 0;
+        int freq_o = 0;
+        int freq_n = 0;
 
-        for(char s : sample){
-            freq.put(s,0);
-        }
         for(int i=0; i<text.length(); i++){
-            char ch = text.charAt(i);
-
-            if(freq.containsKey(ch)){
-                freq.put(ch, freq.get(ch) + 1);
-            }
+            if(text.charAt(i)=='b') freq_b++;
+            else if(text.charAt(i)=='a') freq_a++;
+            else if(text.charAt(i)=='l') freq_l++;
+            else if(text.charAt(i)=='o') freq_o++;
+            else if(text.charAt(i)=='n') freq_n++;
         }
-        freq.put('l',freq.get('l')/2);
-        freq.put('o',freq.get('o')/2);
 
-        return Math.min(
-            Math.min(freq.get('b'), freq.get('a')),
-            Math.min(
-                Math.min(freq.get('l'), freq.get('o')),
-                freq.get('n')
-            )
-        );
-
+        return Math.min(Math.min(freq_b,freq_a),
+        Math.min(freq_n,Math.min(freq_o/2,freq_l/2)));
     }
 }
