8622e4a8c0b835399408bb309d5a5d4c44c6ad5c
Yogesh-Thadke <yogeshthadke22@gmail.com>
2026-07-04 10:27:50 +0530

    Time: 305 ms (51.27%) | Memory: 0B (100.00%) - LeetSync

--- 4195-find-users-with-high-token-usage/find-users-with-high-token-usage.sql
+++ 4195-find-users-with-high-token-usage/find-users-with-high-token-usage.sql
@@ -3,9 +3,5 @@ SELECT user_id, COUNT(prompt) prompt_count, ROUND(AVG(tokens),2) avg_tokens
 FROM prompts p
 GROUP BY user_id
 HAVING  COUNT(prompt) >= 3
-AND AVG(tokens) < ANY (
-    SELECT tokens
-    FROM prompts s
-    WHERE p.user_id = s.user_id
-    )
+AND AVG(tokens) < MAX(tokens)
 ORDER BY AVG(tokens) DESC, user_id ASC
