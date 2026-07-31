f2c166a1c1d211354eb832eb849922411eb117ef
Yogesh-Thadke <yogeshthadke22@gmail.com>
2026-07-31 20:39:25 +0530

    Time: 410 ms (12.91%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select class
-from courses
-group by class
-having count(class) >= 5
+SELECT class
+FROM courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
