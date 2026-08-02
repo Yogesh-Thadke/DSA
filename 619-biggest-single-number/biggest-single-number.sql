d5483db3a0a868d0173cada8a966d71b1ce2fba8
Yogesh-Thadke <yogeshthadke22@gmail.com>
2026-08-02 20:44:39 +0530

    Time: 494 ms (25.13%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
-SELECT ifnull(
-    (SELECT num
+SELECT MAX(num) num
+FROM (
+    SELECT num
     FROM MyNumbers
     GROUP BY num
     HAVING COUNT(num) = 1
-    ORDER BY num DESC
-    LIMIT 1),null) num;
+) t
