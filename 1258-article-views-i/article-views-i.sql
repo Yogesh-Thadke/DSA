0b1d4515a0dfbb4066ba92bf1f25b57ab81dcf72
Yogesh-Thadke <yogeshthadke22@gmail.com>
2026-08-08 10:51:27 +0530

    Time: 505 ms (20.19%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-SELECT DISTINCT author_id id
-FROM views
-where author_id = viewer_id
+SELECT DISTINCT author_id AS id
+FROM Views v1
+WHERE author_id IN (
+    SELECT author_id
+    FROM Views v2
+    WHERE v1.author_id = v2.viewer_id
+)
 ORDER BY author_id ASC;
