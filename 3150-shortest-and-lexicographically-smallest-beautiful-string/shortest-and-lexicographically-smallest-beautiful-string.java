100882d22d798fe5643f61e3d1f1a7b3c5df1c0f
Yogesh-Thadke <yogeshthadke22@gmail.com>
2026-08-26 11:36:57 +0530

    Time: 1 ms (100.00%) | Memory: 44 MB (58.39%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,24 +1,27 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
+        int l = 0;
+        int oneCount = 0;
+
         int minlen = Integer.MAX_VALUE;
         String ans = "";
-        for(int i=0; i<s.length(); i++){
-            int oneCount = 0;
-            for(int j=i; j<s.length(); j++){
-                char c = s.charAt(j);
-                if(c == '1'){
-                    oneCount++;
-                }
-                if(oneCount == k){
-                    String str = s.substring(i,j+1);
-                    int len = (j+1)-i;
 
-                    if(len < minlen || (len == minlen && str.compareTo(ans) < 0)){
-                        ans = str;
-                        minlen = len;
-                    }
-                    break;
+        for(int r=0; r<s.length(); r++){
+            if(s.charAt(r) == '1'){
+                oneCount++;
+            }
+            if(oneCount == k){
+                while(s.charAt(l) == '0'){
+                    l++;
                 }
+                String str = s.substring(l,r+1);
+                int len = r - l + 1;
+                if(len < minlen || (len == minlen && str.compareTo(ans) < 0)){
+                    ans = str;
+                    minlen = len;
+                }
+                l++;
+                oneCount--;
             }
         }
         return ans;
